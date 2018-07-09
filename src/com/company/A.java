1a3f42d19245af223caf414449593f5d74c911e3
Serhii A. Miroshnychenko <s.a.miroshnychenko@globallogic.com>
2018-07-09 14:44:20 +0300

    without double

--- src/com/company/A.java
+++ src/com/company/A.java
@@ -8,17 +8,17 @@ public class A {
     void m(){
         System.out.println("m()");
     }
-    void m(int a){
+    /*void m(int a){
         System.out.println("int a");
-    }
+    }*/
     void m(int a, int b){
         System.out.println("int a, int b");
     }
     void m(int ...a){
         System.out.println("int ...a");
     }
-    double m(double a){
+    /*double m(double a){
         System.out.println("double a");
         return 10.0;
-    }
+    }*/
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,16 +3,10 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        Girl ira = new Girl("Ira", 24);
-        ira.goToCinema();
-        System.out.println(ira.countInts("qwerty",1, 2, 3, 4, 5));
-        System.out.println(ira.goToRest("Mac"));
-        Girl ira1 = new Girl();
-        System.out.println(ira);
-        System.out.println(ira1);
-        ira1.goToCinema();
-        ira1=null;
-        ira=null;
-        System.gc();
+        A a = new A();
+        a.m(0);
+        a.m(1,2);
+        a.m(1,2,3,4,5);
+        /*a.m(1.4);*/
     }
 }
