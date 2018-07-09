0a89dd1578fc5b588359511adf9c4a1bb052e0be
Serhii A. Miroshnychenko <s.a.miroshnychenko@globallogic.com>
2018-07-09 14:52:01 +0300

    long int

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,12 +3,12 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        aMethod((byte) 9);
+        aMethod(9L, 10);
     }
-    public static void  aMethod(byte val){
-        System.out.println("byte");
+    public static void  aMethod(long val1, int val2){
+        System.out.println("long int");
     }
-    public static void  aMethod(short val){
-        System.out.println("short");
+    public static void  aMethod(int val1, long val2){
+        System.out.println("int long");
     }
 }
